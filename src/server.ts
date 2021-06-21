57857343e3dadfb2779885d37225e48526349479
Lucas Mezencio <lucasmezencio@outlook.com>
2021-06-21 09:40:01 -0300

    primeira rota criada

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,16 @@ import express from "express"
 
 const app = express()
 
+/*
+ * GET    => Buscar uma informação
+ * POST   => Inserir (criar) uma informação
+ * PUT    => Alterar uma informação
+ * DELETE => Remover um dado
+ * PATCH  => Alterar uma informação específica
+ */
+
+app.get("/test", (req, res) => {
+    res.send("Olá nlw")
+})
+
 app.listen(3000, () => console.log("server is running"))
