c8a84284175ebe987efda371a91f5ae5909ca314
Lucas Mezencio <lucasmezencio@outlook.com>
2021-06-23 10:33:00 -0300

    banco de dados criado

--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,8 @@
+import "reflect-metadata";
 import express from "express";
 
+import "./database";
+
 const app = express();
-
-/*
- * GET    => Buscar uma informação
- * POST   => Inserir (criar) uma informação
- * PUT    => Alterar uma informação
- * DELETE => Remover um dado
- * PATCH  => Alterar uma informação específica
- */
-
-/**
- * Tipos de parâmetros:
- *
- * Routes params => "Parametro dividido por barra (busca através do recurso: /prod/{id}) "
- * Query params => "busca iniciada após por '?': /prod?name=teclado&..."
- * Body params => "usados nos metodos POST, PUT, PATCH" {
- *  "name": "teclado"
- *  "description": "digita as coisas..."
- * }
- */
-
-app.get("/test", (req, res) => {
-  return res.send("Olá nlw");
-});
-
-app.post("/test-post", (req, res) => {
-  return res.send("Olá nlw: método POST");
-});
 
 app.listen(3000, () => console.log("server is running"));
