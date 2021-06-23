572b351ef29d8d32d09ff69842b1c7ea9bcfa715
Lucas Mezencio <lucasmezencio@outlook.com>
2021-06-23 10:44:41 -0300

    primeira migration

---
+++ src/database/migrations/1624455867138-CreateUsers.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUsers1624455867138 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
