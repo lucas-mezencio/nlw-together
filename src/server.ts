e46a36bbafda7df2957dbc80110a31887e0b8a20
Lucas Mezencio <lucasmezencio@outlook.com>
2021-06-23 14:05:45 -0300

    tratando excecoes

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
-import express from "express";
+import express, { Request, Response, NextFunction, response } from "express";
+import "express-async-errors";
 import { router } from "./routes";
 
 import "./database";
@@ -9,5 +10,18 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      error: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal Server Error",
+  });
+});
 
 app.listen(3000, () => console.log("server is running"));
